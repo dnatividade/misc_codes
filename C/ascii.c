573f7848d8577e7c9ad78e4d70736dd9028ebb46
dnatividade <43869367+dnatividade@users.noreply.github.com>
2018-10-05 07:51:10 -0300

    Create ascii.c

---
+++ C/ascii.c
@@ -0,0 +1,18 @@
+/*
+	Simple example of conversion: STRING to ASCII and HEX
+
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	char *str = argv[1];
+
+	printf("CHAR - ASCII - HEX\n");
+	while(*str) {
+		printf("%c      %u      %x\n", *str, *str, *str);
+		str++;
+	}
+
+	return(0);
+}
