62a063b28b18812c353ed01cd2fda4e8fb809754
dnatividade <43869367+dnatividade@users.noreply.github.com>
2019-08-15 13:14:49 -0300

    Create hex_to_string.cpp

---
+++ C++/functions/hex_to_string.cpp
@@ -0,0 +1,28 @@
+//Convert Hex to String
+#include <algorithm>
+#include <stdexcept>
+
+std::string hex_to_string(const std::string& input)
+{
+    static const char* const lut = "0123456789ABCDEF";
+    size_t len = input.length();
+    if (len & 1) throw std::invalid_argument("odd length");
+
+    std::string output;
+    output.reserve(len / 2);
+    for (size_t i = 0; i < len; i += 2)
+    {
+        char a = input[i];
+        const char* p = std::lower_bound(lut, lut + 16, a);
+        if (*p != a) throw std::invalid_argument("not a hex digit");
+
+        char b = input[i + 1];
+        const char* q = std::lower_bound(lut, lut + 16, b);
+        if (*q != b) throw std::invalid_argument("not a hex digit");
+
+        output.push_back(((p - lut) << 4) | (q - lut));
+    }
+    return output;
+}
+
+//source: https://stackoverflow.com/questions/3381614/c-convert-string-to-hexadecimal-and-vice-versa/11190456
